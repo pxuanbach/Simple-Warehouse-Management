49546362f6e29b2053eee101d9e468faf5358152
UIT.19521334 <67258104+UIT19521334@users.noreply.github.com>
2020-10-13 07:36:27 +0700

    viết code phần acccount
    thêm file txt quản lí account sẽ thêm liên kết tới csdl trong tg tới

---
+++ project/WindowsFormsApp3/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace todo_list2
+{
+    public class Account
+    {
+        private string User;
+        private string EncodePassword;
+        public string user
+        {
+            get
+            {
+                return this.User;
+            }
+            set
+            {
+                this.User = value;
+            }
+        }
+        public string Pass
+        {
+            get
+            {
+                return this.EncodePassword;
+            }
+            set
+            {
+                this.EncodePassword = value;
+            }
+        }
+        public static string Encode(string chuoi)
+        {
+            string str_md5 = "";
+            byte[] mang = System.Text.Encoding.UTF8.GetBytes(chuoi);
+
+            MD5CryptoServiceProvider my_md5 = new MD5CryptoServiceProvider();
+            mang = my_md5.ComputeHash(mang);
+
+            foreach (byte b in mang)
+            {
+                str_md5 += b.ToString("X2");
+            }
+
+            return str_md5;
+        }
+    }
+}
