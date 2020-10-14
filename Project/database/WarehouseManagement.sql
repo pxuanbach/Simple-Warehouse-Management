0dd143f86633c85f9a0dbb12c4f0fdc02f432bb9
Phạm Xuân Bách <55500268+pxuanbach@users.noreply.github.com>
2020-10-15 00:51:35 +0700

    Update database
    cập nhật database: thêm bảng và 1 số thuộc tính cần thiết

--- Project/database/WarehouseManagement.sql
+++ Project/database/WarehouseManagement.sql
@@ -4,20 +4,31 @@ go
 use WarehouseManagement
 go
 
-create table STOCK
+create table USERACC
 (
-	IDSTOCK varchar(20) primary key,	--Ma hang	
-	NAMESTOCK varchar(100),				--Ten hang
-	UNIT varchar(10),					--Don vi
-	AMOUNT int,							--So luong
-	PRICE money,						--Don gia
-	DATEADDED smalldatetime,			--Ngay nhap
-	MANUFACTURINGDATE smalldatetime,	--Ngay san xuat
-	EXPIRYDATE smalldatetime,			--Han su dung
+	USERID int primary key,
+	USERNAME varchar(20),
+	USERPASS varchar(20),
 )
 
-create table USERACCOUNT
+create table HANGHOA
 (
-	USERNAME varchar(20) primary key,
-	PASSWORD varchar(20),
+	MAHANG varchar(20) primary key,	--Ma hang	
+	TENHANG varchar(100),				--Ten hang
+	DVT varchar(10),					--Don vi
+	SOLUONG int,							--So luong
+	GIA money,						--Don gia
+	NGNHAP smalldatetime,			--Ngay nhap
+	NGSANXUAT smalldatetime,	--Ngay san xuat
+	HSD smalldatetime,			--Han su dung
+	USERID int foreign key references USERACC(USERID),
 )
+
+create table ORDERS
+(
+	MAHOADON varchar(20) primary key,
+	NGXUAT smalldatetime,
+	MAHANG varchar(20) foreign key references HANGHOA(MAHANG),
+	USERID int foreign key references USERACC(USERID),
+)
+
