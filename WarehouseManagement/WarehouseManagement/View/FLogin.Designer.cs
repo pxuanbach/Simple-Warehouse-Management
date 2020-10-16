82a1c12dcb73d33cb1034b78d8917905478d94d8
Phạm Xuân Bách <55500268+pxuanbach@users.noreply.github.com>
2020-10-16 11:11:28 +0700

    Xây dựng mô hình MVC

---
+++ WarehouseManagement/WarehouseManagement/View/FLogin.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class FLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUPass = new System.Windows.Forms.TextBox();
+            this.buttonSignIn = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonSignUp = new System.Windows.Forms.Button();
+            this.checkBoxPass = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User";
+            this.label1.UseMnemonic = false;
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxUName
+            // 
+            this.textBoxUName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUName.Location = new System.Drawing.Point(61, 82);
+            this.textBoxUName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUName.Name = "textBoxUName";
+            this.textBoxUName.Size = new System.Drawing.Size(288, 28);
+            this.textBoxUName.TabIndex = 1;
+            this.textBoxUName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 135);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // textBoxUPass
+            // 
+            this.textBoxUPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUPass.Location = new System.Drawing.Point(61, 162);
+            this.textBoxUPass.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUPass.Name = "textBoxUPass";
+            this.textBoxUPass.Size = new System.Drawing.Size(288, 28);
+            this.textBoxUPass.TabIndex = 3;
+            // 
+            // buttonSignIn
+            // 
+            this.buttonSignIn.AutoEllipsis = true;
+            this.buttonSignIn.BackColor = System.Drawing.Color.LightGreen;
+            this.buttonSignIn.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSignIn.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.buttonSignIn.ImageKey = "(none)";
+            this.buttonSignIn.Location = new System.Drawing.Point(61, 256);
+            this.buttonSignIn.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSignIn.Name = "buttonSignIn";
+            this.buttonSignIn.Size = new System.Drawing.Size(288, 63);
+            this.buttonSignIn.TabIndex = 4;
+            this.buttonSignIn.Text = "SIGN IN";
+            this.buttonSignIn.UseVisualStyleBackColor = false;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.Location = new System.Drawing.Point(257, 327);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(92, 40);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonSignUp
+            // 
+            this.buttonSignUp.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSignUp.Location = new System.Drawing.Point(61, 328);
+            this.buttonSignUp.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSignUp.Name = "buttonSignUp";
+            this.buttonSignUp.Size = new System.Drawing.Size(103, 39);
+            this.buttonSignUp.TabIndex = 6;
+            this.buttonSignUp.Text = "Sign up";
+            this.buttonSignUp.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPass
+            // 
+            this.checkBoxPass.AutoSize = true;
+            this.checkBoxPass.Location = new System.Drawing.Point(103, 374);
+            this.checkBoxPass.Name = "checkBoxPass";
+            this.checkBoxPass.Size = new System.Drawing.Size(195, 21);
+            this.checkBoxPass.TabIndex = 7;
+            this.checkBoxPass.Text = "Remember your password";
+            this.checkBoxPass.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(211, 219);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 21);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "User";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(276, 219);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 21);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Admin";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // FLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(401, 432);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.checkBoxPass);
+            this.Controls.Add(this.buttonSignUp);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSignIn);
+            this.Controls.Add(this.textBoxUPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxUName);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FLogin";
+            this.Text = "Sign In";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxUPass;
+        private System.Windows.Forms.Button buttonSignIn;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonSignUp;
+        private System.Windows.Forms.CheckBox checkBoxPass;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
