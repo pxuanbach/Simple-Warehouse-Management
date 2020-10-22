1efb016aa31f72f973be36215c1af432533e88f5
Phạm Xuân Bách <55500268+pxuanbach@users.noreply.github.com>
2020-10-22 14:54:15 +0700

    Upload Project

---
+++ WarehouseManagement/WarehouseManagement/Model/Process.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Concurrent;
+
+namespace WarehouseManagement.Model
+{
+    class Process
+    {
+        public static SqlConnection conn;
+        public static SqlDataAdapter da;
+        public static SqlCommand cmd;
+        public static DataTable data;
+        public static SqlDataReader reader;
+        public static string connectLink = @"Server=LAPTOP-ADHV1Q9P\SQLEXPRESS;Database=WarehouseManagement;User Id=sa;Password=1;";
+
+        public static DataTable createTable(string sql)
+        {
+            conn = new SqlConnection(connectLink);
+            conn.Open();
+            da = new SqlDataAdapter(sql, conn);
+            data = new DataTable();
+            da.Fill(data);
+            conn.Close();
+            return data;
+        }
+        public static int ExecutiveNonQuery(string sql)
+        {
+            conn = new SqlConnection(connectLink);
+            conn.Open();
+            cmd = new SqlCommand(sql, conn);
+            int rs = cmd.ExecuteNonQuery();
+            conn.Close();
+            return rs;
+        }
+
+        public static int ExecutiveReader(string sql)
+        {
+            int ck = 0;
+            conn = new SqlConnection(connectLink);
+            conn.Open();
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+            while (reader.HasRows)
+            {
+                if (reader.Read() == false) break;
+                else
+                    ck++;
+            }
+            conn.Close();
+            return ck;
+        }
+
+        public static int GetUkey(string sql)
+        {
+            int i = 0;
+            conn = new SqlConnection(connectLink);
+            conn.Open();
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+            while (reader.HasRows)
+            {
+                if (reader.Read() == false) break;
+                i = reader.GetInt32(0);
+            }
+            return i;
+        }
+
+        public static string[] ListData(string sql)
+        {
+            string[] list = new string[10];
+            conn = new SqlConnection(connectLink);
+            conn.Open();
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+            while (reader.HasRows)
+            {
+                if (reader.Read() == false) break;
+                list[0] = reader[0].ToString();
+                list[1] = reader.GetString(1);
+                //if (reader[2] == null)
+                //    list[2] = "None";
+                //else
+                //{
+                //    list[2] = reader.GetString(2);
+                //}    
+                    
+                //if (reader.GetString(3) == null)
+                //    list[3] = "";
+                //else
+                //    list[3] = reader.GetString(3);
+                //if (reader.GetString(4) == null)
+                //    list[4] = "";
+                //else
+                //    list[4] = reader.GetString(4);
+                //list[4] = reader.GetString(4);
+                //list[5] = reader.GetString(5);
+            }
+            conn.Close();
+            return list;
+        }
+        public static DateTime date(string sql)
+        {
+            DateTime time = new DateTime();
+            conn = new SqlConnection(connectLink);
+            conn.Open();
+            cmd = new SqlCommand(sql, conn);
+            reader = cmd.ExecuteReader();
+            while (reader.HasRows)
+            {
+                if (reader.Read() == false) break;
+                else
+                {
+                    time = reader.GetDateTime(0);
+                }
+            }
+            conn.Close();
+            return time;
+        }
+    }
+}
---
+++ WarehouseManagement/WarehouseManagement/View/FMain/StockPage.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace WarehouseManagement
+{
+    partial class StockPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Search = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MAHANG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TENHANG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DVT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SOLUONG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DONGIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NGNHAP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NSX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HSD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btEdit = new System.Windows.Forms.Button();
+            this.btPrint = new System.Windows.Forms.Button();
+            this.btDel = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 20;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Name\t",
+            "Goods ID",
+            "Date"});
+            this.comboBox1.Location = new System.Drawing.Point(45, 357);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(224, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Search
+            // 
+            this.Search.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Search.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Search.Location = new System.Drawing.Point(275, 352);
+            this.Search.Margin = new System.Windows.Forms.Padding(4);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(521, 34);
+            this.Search.TabIndex = 1;
+            this.Search.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MAHANG,
+            this.TENHANG,
+            this.DVT,
+            this.SOLUONG,
+            this.DONGIA,
+            this.NGNHAP,
+            this.NSX,
+            this.HSD});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 324);
+            this.dataGridView1.TabIndex = 20;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // MAHANG
+            // 
+            this.MAHANG.DataPropertyName = "MAHANG";
+            this.MAHANG.HeaderText = "Mã Hàng";
+            this.MAHANG.MinimumWidth = 6;
+            this.MAHANG.Name = "MAHANG";
+            // 
+            // TENHANG
+            // 
+            this.TENHANG.DataPropertyName = "TENHANG";
+            this.TENHANG.HeaderText = "Tên Hàng";
+            this.TENHANG.MinimumWidth = 6;
+            this.TENHANG.Name = "TENHANG";
+            // 
+            // DVT
+            // 
+            this.DVT.DataPropertyName = "DVT";
+            this.DVT.HeaderText = "Đơn vị";
+            this.DVT.MinimumWidth = 6;
+            this.DVT.Name = "DVT";
+            // 
+            // SOLUONG
+            // 
+            this.SOLUONG.DataPropertyName = "SOLUONG";
+            this.SOLUONG.HeaderText = "Số Lượng";
+            this.SOLUONG.MinimumWidth = 6;
+            this.SOLUONG.Name = "SOLUONG";
+            // 
+            // DONGIA
+            // 
+            this.DONGIA.DataPropertyName = "DONGIA";
+            this.DONGIA.HeaderText = "Đơn Giá";
+            this.DONGIA.MinimumWidth = 6;
+            this.DONGIA.Name = "DONGIA";
+            // 
+            // NGNHAP
+            // 
+            this.NGNHAP.DataPropertyName = "NGNHAP";
+            this.NGNHAP.HeaderText = "Ngày Nhập";
+            this.NGNHAP.MinimumWidth = 6;
+            this.NGNHAP.Name = "NGNHAP";
+            // 
+            // NSX
+            // 
+            this.NSX.DataPropertyName = "NGSANXUAT";
+            this.NSX.HeaderText = "NSX";
+            this.NSX.MinimumWidth = 6;
+            this.NSX.Name = "NSX";
+            // 
+            // HSD
+            // 
+            this.HSD.DataPropertyName = "HSD";
+            this.HSD.HeaderText = "HSD";
+            this.HSD.MinimumWidth = 6;
+            this.HSD.Name = "HSD";
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.btCancel);
+            this.panel2.Controls.Add(this.btSave);
+            this.panel2.Controls.Add(this.btEdit);
+            this.panel2.Controls.Add(this.btPrint);
+            this.panel2.Controls.Add(this.btDel);
+            this.panel2.Controls.Add(this.btAdd);
+            this.panel2.Location = new System.Drawing.Point(0, 406);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1000, 140);
+            this.panel2.TabIndex = 9;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCancel.Image = global::WarehouseManagement.Properties.Resources.cancel__2_;
+            this.btCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btCancel.Location = new System.Drawing.Point(486, 44);
+            this.btCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(153, 67);
+            this.btCancel.TabIndex = 9;
+            this.btCancel.Text = "      CANCEL";
+            this.btCancel.UseMnemonic = false;
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // btSave
+            // 
+            this.btSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSave.Image = global::WarehouseManagement.Properties.Resources.save;
+            this.btSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btSave.Location = new System.Drawing.Point(329, 44);
+            this.btSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(149, 67);
+            this.btSave.TabIndex = 8;
+            this.btSave.Text = "    SAVE";
+            this.btSave.UseMnemonic = false;
+            this.btSave.UseVisualStyleBackColor = true;
+            // 
+            // btEdit
+            // 
+            this.btEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEdit.Image = global::WarehouseManagement.Properties.Resources.ICEdit;
+            this.btEdit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btEdit.Location = new System.Drawing.Point(172, 44);
+            this.btEdit.Margin = new System.Windows.Forms.Padding(4);
+            this.btEdit.Name = "btEdit";
+            this.btEdit.Size = new System.Drawing.Size(149, 67);
+            this.btEdit.TabIndex = 4;
+            this.btEdit.Text = "   EDIT";
+            this.btEdit.UseMnemonic = false;
+            this.btEdit.UseVisualStyleBackColor = true;
+            this.btEdit.Click += new System.EventHandler(this.btEdit_Click);
+            // 
+            // btPrint
+            // 
+            this.btPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btPrint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPrint.Image = global::WarehouseManagement.Properties.Resources.ICprin;
+            this.btPrint.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btPrint.Location = new System.Drawing.Point(835, 44);
+            this.btPrint.Margin = new System.Windows.Forms.Padding(4);
+            this.btPrint.Name = "btPrint";
+            this.btPrint.Size = new System.Drawing.Size(149, 67);
+            this.btPrint.TabIndex = 6;
+            this.btPrint.Text = "     PRINT";
+            this.btPrint.UseMnemonic = false;
+            this.btPrint.UseVisualStyleBackColor = true;
+            // 
+            // btDel
+            // 
+            this.btDel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btDel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDel.Image = global::WarehouseManagement.Properties.Resources.ICDel;
+            this.btDel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btDel.Location = new System.Drawing.Point(678, 44);
+            this.btDel.Margin = new System.Windows.Forms.Padding(4);
+            this.btDel.Name = "btDel";
+            this.btDel.Size = new System.Drawing.Size(149, 67);
+            this.btDel.TabIndex = 5;
+            this.btDel.Text = "    DELETE";
+            this.btDel.UseMnemonic = false;
+            this.btDel.UseVisualStyleBackColor = true;
+            this.btDel.Click += new System.EventHandler(this.btDel_Click);
+            // 
+            // btAdd
+            // 
+            this.btAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAdd.Image = global::WarehouseManagement.Properties.Resources.ICAdd;
+            this.btAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btAdd.Location = new System.Drawing.Point(15, 44);
+            this.btAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(149, 67);
+            this.btAdd.TabIndex = 3;
+            this.btAdd.Text = "   ADD";
+            this.btAdd.UseMnemonic = false;
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.BtAdd_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(267, 386);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(532, 1);
+            this.panel1.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = global::WarehouseManagement.Properties.Resources.IcSearch1;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.Location = new System.Drawing.Point(804, 352);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 37);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "  Search";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // StockPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "StockPage";
+            this.Size = new System.Drawing.Size(1004, 550);
+            this.Load += new System.EventHandler(this.StockUser_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox Search;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btEdit;
+        private System.Windows.Forms.Button btPrint;
+        private System.Windows.Forms.Button btDel;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MAHANG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TENHANG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DVT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SOLUONG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DONGIA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NGNHAP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NSX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HSD;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Button btSave;
+    }
+}
